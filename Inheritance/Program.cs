cbe39bf36316e20ba703ff789c86c54f65010a47
Maverick Grayer <maverick72032@gmail.com>
2024-03-22 17:10:18 -0400

    cleaned code

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,35 +6,6 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            // TODO Be sure to follow best practice when creating your classes
-
-            // Create a class Animal
-            // give this class 4 members that all Animals have in common
-            //COMPLETED
-
-            // Create a class Bird
-            // give this class 4 members that are specific to Bird
-            // Set this class to inherit from your Animal Class
-            //COMPLETED
-            // Create a class Reptile
-            // give this class 4 members that are specific to Reptile
-            // Set this class to inherit from your Animal Class
-            //COMPLETED
-
-
-
-            /*Create an object of your Bird class
-             *  give values to your members using the object of your Bird class
-             *  
-             * Creatively display the class member values 
-             */
-
-            /*Create an object of your Reptile class
-             *  give values to your members using the object of your Reptile class
-             *  
-             * Creatively display the class member values 
-             */
-
             Bird bird1 = new Bird()
             {
                 Age = 2,
